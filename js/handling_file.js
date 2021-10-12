3d581f482c6ceb7bd488a609b21d798110ed9608
YgorJ <ygor-skat@hotmail.com>
2021-10-12 19:00:31 -0300

    Update handling_file.js

--- js/handling_file.js
+++ js/handling_file.js
@@ -1,6 +1,8 @@
 var file = document.getElementById("load-file");
-var reader = new FileReader();
-reader.addEventListener('load', (event) => {
-    img.src = event.target.result;
-});
-reader.readAsDataURL(file);
+function readFile(file) {
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+      img.src = event.target.result;
+    });
+    reader.readAsDataURL(file);
+}
